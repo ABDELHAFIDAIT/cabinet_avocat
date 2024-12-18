afae5c6f381dead92c20cb8471ae060dc2b7e112
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-18 22:52:04 +0100

    Créer la page dashboard Client

---
+++ views/client_dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
+    <title>LexAdvisor - Client Dashborad</title>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+    <header class="px-5 md:px-10 lg:px-16 py-5">
+        <nav>
+            <div class="flex gap-5 items-center">
+                <ion-icon class="text-4xl cursor-pointer" name="menu"></ion-icon>
+                <div class="flex items-center gap-2 cursor-pointer">
+                    <img class="h-8" src="../assets/img/logo.png" alt="Logo du Site LexAdvisor">
+                    <h1 class="text-xl font-semibold">Lex<span class="text-[#01FF70]">Advisor</span></h1>
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </nav>
+    </header>
+
+
+
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
