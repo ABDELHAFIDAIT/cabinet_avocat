abe04b4a63d238b26e92a7dc785e269ee0b07017
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-19 10:39:56 +0100

    Ajouter le Header aux Dashboard Admin et Client

---
+++ views/avocat_dashboard.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
+    <title>LexAdvisor - Avocat Dashborad</title>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+    <!-- HEADER SECTON -->
+    <header class="px-5 md:px-10 lg:px-16 py-5 bg-[#02101f] shadow-lg">
+        <nav class="flex justify-between items-center">
+            <div class="flex gap-5 items-center">
+                <div class="flex items-center gap-2 cursor-pointer">
+                    <img class="h-8" src="../assets/img/logo.png" alt="Logo du Site LexAdvisor">
+                    <h1 class="text-xl font-semibold">Lex<span class="text-[#01FF70]">Advisor</span></h1>
+                </div>
+            </div>
+            <div class="flex items-center gap-2">
+                <a href="#" class="text-purple-200 underline duration-500 hover:text-purple-400">Déconnexion</a>
+                <a href="#"><img class="rounded-full border-2 border-[#01FF70] w-14" src="../assets/img/lawyer.jpg" alt="Image du Client"></a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+
+
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
--- views/client_dashboard.php
+++ views/client_dashboard.php
@@ -11,20 +11,25 @@
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
-    <header class="px-5 md:px-10 lg:px-16 py-5">
-        <nav>
+    <!-- HEADER SECTON -->
+    <header class="px-5 md:px-10 lg:px-16 py-5 bg-[#02101f] shadow-lg">
+        <nav class="flex justify-between items-center">
             <div class="flex gap-5 items-center">
-                <ion-icon class="text-4xl cursor-pointer" name="menu"></ion-icon>
                 <div class="flex items-center gap-2 cursor-pointer">
                     <img class="h-8" src="../assets/img/logo.png" alt="Logo du Site LexAdvisor">
                     <h1 class="text-xl font-semibold">Lex<span class="text-[#01FF70]">Advisor</span></h1>
                 </div>
             </div>
-            <div>
-                
+            <div class="flex items-center gap-2">
+                <a href="#" class="text-purple-200 underline duration-500 hover:text-purple-400">Déconnexion</a>
+                <a href="#"><img class="rounded-full border-2 border-[#01FF70] w-14" src="../assets/img/client.jpg" alt="Image du Client"></a>
             </div>
         </nav>
     </header>
+
+    <main>
+
+    </main>
 
 
 
